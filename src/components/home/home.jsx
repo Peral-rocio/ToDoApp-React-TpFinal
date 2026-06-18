f8fc88a9597168dbedb651f4c076f341e1fe24f8
Peralta Rocio <peralta.rocio.2229@gmail.com>
2026-06-17 23:32:28 -0300

    Cambio total de estrucutra maquetado de home

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,37 +1,42 @@
 import styles from "./home.module.css"
-import { Settings } from "lucide-react";
-import pandaBusca from "../../assets/pandaBusca.png"
-import tupuedesPanda from "../../assets/tupuedesPanda.png"
+import { Settings, Trash2, Check, House, FilePlus} from "lucide-react";
+import pandaBusca from "../../assets/pandaBusca.png";
+import bambooTask from "../../assets/bambooTask.png";
+import { Plus } from "lucide-react";
+import { Link } from "react-router-dom";
 
 
 function Home (){
 return <>
   <header className={styles.headerHome}>
-    <button className={styles.buttonClose}> 
-       <Settings />
-    </button>
+    <img className={styles.logoBambooTask} src={bambooTask} alt="logo panda feliz" />
   </header>
    
    <main className={styles.main}>
     <div className={styles.conteinerTareas}>
-    <h1>
+    <h1 className={styles.h1Tareas}>
         Tareas
     </h1>
     <div className={styles.inputTareas}>
         <input className={styles.tarea} type="text" />
-         <input className={styles.tarea} type="text" />
-          <input className={styles.tarea} type="text" />
-           <input className={styles.tarea} type="text" />
-            <input className={styles.tarea} type="text" />
-             <input className={styles.tarea} type="text" />
+        <button className={styles.tareaCheck}>
+            <Check/>
+        </button>
+        <button className={styles.tareaTrash}>
+            <Trash2/>
+        </button>
     </div> 
 
     </div>
     <div className={styles.conteinerFiltro}>
-        <h1>
-            filtrar Tarea
+        <h1 className= {styles.h1Tareas}>
+            Filtrar Tarea
         </h1>
-        <input className={styles.filtro} type="text" />
+        <select className={styles.SelectContenedor} >
+            <option value="Todas">Todas</option>
+            <option value="Completas">Completas</option>
+            <option value="Incompletas">Incompletas</option>
+        </select>
    <div className={styles.conteinerImg}>
     <img  className={styles.pandaBusca} src={pandaBusca} alt="pandabusca" />
    </div>
@@ -39,12 +44,25 @@ return <>
    </main>
 
    <footer className={styles.footer}>
-    <img className={styles.tupuedes} src={tupuedesPanda} alt="panda de tu puedes lograrlo" />
-    <button className={styles.botonAgregar}> Agregar tarea </button>
+    <div className={styles.contenedorNav}>
+       <Link to="/home" className={styles.iconosNav}> 
+       <House size={40}/>
+    </Link>
+    </div>
+     <div className={styles.contenedorNav}>
+       <Link to ="" className={styles.iconosNav}> 
+       <FilePlus size={40}/>
+    </Link>
+    </div>
+     <div className={styles.contenedorNav}>
+       <Link to="" className={styles.iconosNav}> 
+       <Settings size={40}/>
+    </Link>
+    </div>
    </footer>
-
 
 </>
 }
+
 
 export default Home;
