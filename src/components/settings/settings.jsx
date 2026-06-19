44702dc9fe73d066bc5751f3ceb62d404ed74724
Peralta Rocio <peralta.rocio.2229@gmail.com>
2026-06-19 00:42:05 -0300

    maquetado finalizado de settings

--- src/components/settings/settings.jsx
+++ src/components/settings/settings.jsx
@@ -1,12 +1,43 @@
+import styles from "./settings.module.css";
+import bambooTask from "../../assets/bambooTask.png";
+import { SunMoon, Calendar1 } from "lucide-react";
+import panditaCool from "../../assets/panditaCool.png";
 
-
-
-function Settings (){
-return (
+function Settings() {
+  return (
     <>
-    <h1>HOLA ESTO ES SETTINGS</h1>
+      <header className={styles.headerSettings}>
+        <img
+          className={styles.bambooSettings}
+          src={bambooTask}
+          alt="logo panda feliz"
+        />
+      </header>
+
+      <div className={styles.padreSettings}>
+        <div className={styles.hijoSettings}>
+          <div className={styles.modoClaro}>
+            <SunMoon />
+          </div>
+          <div className={styles.calendario}>
+            <Calendar1 />
+          </div>
+        </div>
+        <div className={styles.hijoSettings}>
+          <div className={styles.buttonClose}>
+            <button>cerrar sesion</button>
+          </div>
+          <div className={styles.pandaCool}>
+            <img
+              className={styles.imgPanditaCool}
+              src={panditaCool}
+              alt="panda con lentes de sol"
+            />
+          </div>
+        </div>
+      </div>
     </>
-)
+  );
 }
 
 export default Settings;
